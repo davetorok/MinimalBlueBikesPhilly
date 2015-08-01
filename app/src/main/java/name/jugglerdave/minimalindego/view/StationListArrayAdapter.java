fbeb50911715597e46ecf901d3da6827554edecf
Dave Torok <jugglerdev@gmail.com>
2015-07-31 20:16:45 -0400

    Fixing bearing arrow density.  seems to be fully working, will add padding next

--- app/src/main/java/name/jugglerdave/minimalindego/view/StationListArrayAdapter.java
+++ app/src/main/java/name/jugglerdave/minimalindego/view/StationListArrayAdapter.java
@@ -154,13 +154,15 @@ public class StationListArrayAdapter extends ArrayAdapter<Station>
 
             BitmapFactory.Options options = new BitmapFactory.Options();
             options.inScaled = false;
-           Bitmap orig_blue_arrow = BitmapFactory.decodeResource(viewHolder.bearingImageView.getResources(), R.drawable.bearingarrow,options);
+           Bitmap orig_blue_arrow = BitmapFactory.decodeResource(viewHolder.bearingImageView.getResources(), R.drawable.bearingarrow,options); //todo go back to nondebug
             nav_arrow_orig_width = orig_blue_arrow.getWidth();
             nav_arrow_orig_height = orig_blue_arrow.getHeight();
         }
 
         if (current_location_orig_height < 0 || current_location_orig_width < 0) {
-            Bitmap orig_blue_arrow = BitmapFactory.decodeResource(viewHolder.bearingImageView.getResources(), R.drawable.currentlocationicon);
+            BitmapFactory.Options options2 = new BitmapFactory.Options();
+            options2.inScaled = false;
+            Bitmap orig_blue_arrow = BitmapFactory.decodeResource(viewHolder.bearingImageView.getResources(), R.drawable.currentlocationicon,options2);
             current_location_orig_width = orig_blue_arrow.getWidth();
             current_location_orig_height = orig_blue_arrow.getHeight();
         }
@@ -168,16 +170,17 @@ public class StationListArrayAdapter extends ArrayAdapter<Station>
         Matrix matrix = new Matrix();
         viewHolder.bearingImageView.setScaleType(ImageView.ScaleType.MATRIX);   //required
 
+        int x_offset = getContext().getResources().getDimensionPixelOffset(R.dimen.bearing_arrow_x_offset);
+        int y_offset = getContext().getResources().getDimensionPixelOffset(R.dimen.bearing_arrow_y_offset);
 
         if (MinimalBlueBikesApplication.getGridMilesDistanceFromCurrent(this_station) > 0.05)
         {
-            int density = getContext().getResources().getDimensionPixelOffset(R.dimen.activity_vertical_margin) / 4; // TODO this is now a multiplier of 4. should change to the float multiplier
-            viewHolder.bearingImageView.setImageResource(R.drawable.bearingarrow);
-            matrix.preScale((float) 20.0 / nav_arrow_orig_width, (float) 20.0 / nav_arrow_orig_height);  //(try smaller scaling, was 30 / 20)
+            viewHolder.bearingImageView.setImageResource(R.drawable.bearingarrow); //todo go back to nondebug
+            matrix.preScale((float) 30.0 / nav_arrow_orig_width, (float) 30.0 / nav_arrow_orig_height);  //(try smaller scaling, was 30 / 20)
             //adjust bearing to account for philly grid tilt
             //testing density multiplier
             // should the offset be 15,15?  orig. 15,10
-            matrix.postRotate((float) MinimalBlueBikesApplication.getBearingToFromCurrent(this_station) - ((float) 90.0 + (float) MinimalBlueBikesApplication.phila_map_tilt_degrees), density*10, density*10);
+            matrix.postRotate((float) MinimalBlueBikesApplication.getBearingToFromCurrent(this_station) - ((float) 90.0 + (float) MinimalBlueBikesApplication.phila_map_tilt_degrees), x_offset,y_offset);
 
         } else {
             // current row is the current station location
